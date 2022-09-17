5b69a472f5924b9acc2712eea0c8d0a9cecaf8ae
jobayercse71 <jobayercse71.com>
2022-09-17 21:36:34 +0600

    validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { writeFile } = require('fs');
+const { writeFile, writeFileSync } = require('fs');
 const path = require('path');
 const userDb= require('../user.json');
 
@@ -6,12 +6,13 @@ const userDb= require('../user.json');
 const {users}=userDb;
 
 const getAllUser=(req,res)=>{
-    res.sendFile(path.join(__dirname, "../user.json"));
-}
+    const userLimit =  users.slice(0,3);
+    res.send({users:userLimit})
+    }
 
-const getRandomUser=(req,res)=>{
+    const getRandomUser=(req,res)=>{
     const id= parseInt(req.params.id);
-  
+
     const randomUser = users.filter(user=>user.Id===id);
     res.send(randomUser);
 }
@@ -20,18 +21,30 @@ const getRandomUser=(req,res)=>{
 const saveUser=(req,res)=>{
 
     const info = req.body;
-    const newUser = {users:[...users,info]};
-    writeFile(path.join(__dirname,"../user.json") ,JSON.stringify(newUser),(err)=>{
-            res.send(err)
-    })
-    res.send(newUser)
+    if(info.name && info.address && info.contact && info.gender){
+        const haveUsers = users.find(user=>user.Id===info.Id);
+    if(haveUsers){
+        res.send('Provide unique Id');
+    }
+    else{
+        const newUser = {users:[...users,info]};
+            writeFileSync(path.join(__dirname,"../user.json") ,JSON.stringify(newUser),(err)=>{
+                    res.send(err)
+            })
+        res.send(newUser)
+    }
+    }
+    else{
+        res.send('all data hasnot properly')
+    }
+ 
 }
 
 const updateUser=(req,res)=>{
 
         const body = req.body;
-        // console.log(body);
-        const id = parseInt(req.params.id);
+        
+        const Id = parseInt(req.params.id);
 
         // first option
 
@@ -47,25 +60,57 @@ const updateUser=(req,res)=>{
 //      ]};
 
         // 2nd option
+        const haveUsers = users.find(user=>user.Id===Id);
+        if(haveUsers){
+
+            const restUser = users.filter(user=>user.Id !== Id);
+
+            restUser.push({
+            Id:Id,
+            gender: body.gender,
+            name: body.name,
+            contact: body.contact,
+            address: body.address,
+            photUrl: body.photUrl});
+            const modifyUser = {users:restUser}
+    
+    
+        writeFileSync(path.join(__dirname,"../user.json") ,JSON.stringify(modifyUser),(err)=>{
+            res.send(err)
+    });
+    
+       res.send(modifyUser);
+        }
+        else{
+            res.send('Sorry your provide id is wrong')
+        }
 
 
-        const restUser = users.filter(user=>user.Id !== id);
-
-        restUser.push({
-        Id:id,
-        gender: body.gender,
-        name: body.name,
-        contact: body.contact,
-        address: body.address,
-        photUrl: body.photUrl});
-        const modifyUser = {users:restUser}
+}
 
 
-    writeFile(path.join(__dirname,"../user.json") ,JSON.stringify(modifyUser),(err)=>{
-        res.send(err)
-});
-//    console.log(updatedUser);
-   res.send(modifyUser)
+const deleteUser=(req,res)=>{
+
+    const Id = parseInt(req.params.id);
+
+    const haveUsers = users.find(user=>user.Id===Id);
+    if(haveUsers){
+        const deletedUser = users.filter(user=>user.Id !== Id);
+        const restUser = {users:deletedUser};
+    
+        
+        writeFileSync(path.join(__dirname,"../user.json") ,JSON.stringify(restUser),(err)=>{
+            res.send(err)
+        });
+        res.send(restUser);
+    }
+    else{
+        res.send('Sorry your provide id is wrong so cannot deleted')
+
+    }
+    
+
+
 
 }
 
@@ -75,5 +120,6 @@ module.exports={
     getAllUser,
     getRandomUser,
     saveUser,
-    updateUser
+    updateUser,
+    deleteUser
 }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getAllUser, getRandomUser, saveUser, updateUser } = require('../../controllers/user.controller');
+const { getAllUser, getRandomUser, saveUser, updateUser, deleteUser } = require('../../controllers/user.controller');
 const router = express.Router();
 
 router.get('/all',getAllUser);
 router.get('/:id',getRandomUser);
 router.post('/',saveUser);
 router.patch('/:id',updateUser);
+router.delete('/:id',deleteUser);
 
 
 module.exports=router;
